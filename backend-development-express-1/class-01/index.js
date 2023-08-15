abeb6a54ae8fef8e16b0d9eabefe5838c2d7135f
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-08-15 17:52:18 +0530

    class 1 is complite

--- backend-development-express-1/class-01/index.js
+++ backend-development-express-1/class-01/index.js
@@ -1,1 +1,41 @@
-const mama = anuj;
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.send("this is home page babby");
+})
+
+app.listen(3000, () => {
+    console.log("app started at port number 3000");
+})
+
+// app.post("/api/cars", (req, res) => {
+//     const {name, brand} = req.body;
+//     console.log(name);
+//     console.log(brand);
+//     res.send("name and brand submited succesfully...!");
+// })
+
+app.post("/api/about", (req, res) => {
+    const {name, group} = req.body;
+    console.log(name);
+    console.log(group);
+    console.log("about page submited successfully");
+})
+
+
+// Connect to the database
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1/anujBataBase", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then( () => {
+    console.log("database is connected...!");
+})
+.catch( (err) => {
+    console.log("database is not connected", err);
+});
